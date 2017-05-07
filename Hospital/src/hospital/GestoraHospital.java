6f487b3ecb6ed6d4a60b33bc4d32b799286512e1
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2017-05-07 22:19:35 +0200

    Gestora wena prueba binario

--- Hospital/src/hospital/GestoraHospital.java
+++ Hospital/src/hospital/GestoraHospital.java
@@ -23,6 +23,10 @@ public class GestoraHospital
 		
 		File pepejava = new File ("pepejava.dat");
 		
+		FileInputStream fis = null;
+		ObjectInputStream ois = null;
+		RandomAccessFile ram = null;
+		
 		Persona p0 = null;
 		Persona p1 = null;
 		Persona p2 = null;
@@ -32,20 +36,22 @@ public class GestoraHospital
 		
 		try
 		{
-			
 			FileOutputStream fos = new FileOutputStream (pepejava);
 			ObjectOutputStream oos = new ObjectOutputStream (fos);
+			ram = new RandomAccessFile (pepejava, "rw");
 
-				p0 = new Persona ("Pablo", "Chavalator1", 16, "77925631", 'h', "954456545", new Domicilio ("Pepejava1 Street", "El kelo", 3));
-				p1 = new Persona ("Juan", "Chavalator2", 17, "77924651", 'h', "954236545", new Domicilio ("Pepejava2 Street", "El kelo", 4));
-				p2 = new Persona ("Nzhdeh", "Chavalator3", 18, "77225651", 'h', "952256545", new Domicilio ("Pepejava3 Street", "El kelo", 5));
-				p3 = new Persona ("Firewall", "Chavalator4", 19, "71925651", 'h', "954266545", new Domicilio ("Pepejava4 Street", "El kelo", 6));
+			p0 = new Persona ("Pablo", "Chavalator1", 16, "77925651H", 'h', "954456545", new Domicilio ("Pepejava1 Street", "El kelo", 3));
+			p1 = new Persona ("Juan", "Chavalator2", 17, "77925651H", 'h', "954236545", new Domicilio ("Pepejava2 Street", "El kelo", 4));
+			p2 = new Persona ("Nzhdeh", "Chavalator3", 18, "77925651H", 'h', "952256545", new Domicilio ("Pepejava3 Street", "El kelo", 5));
+			p3 = new Persona ("Firewall", "Chavalator4", 19, "77925651H", 'h', "954266545", new Domicilio ("Pepejava4 Street", "El kelo", 6));
 				
-			
 			oos.writeObject(p0);
 			oos.writeObject(p1);
 			oos.writeObject(p2);
 			oos.writeObject(p3);
+			
+			
+			System.out.println(ram.getFilePointer());
 			
 			oos.close();
 			
@@ -61,17 +67,16 @@ public class GestoraHospital
 			System.out.println(e);
 		}
 		
-		
 		try
 		{
-			FileInputStream fis = new FileInputStream (pepejava);
-			ObjectInputStream ois = new ObjectInputStream (fis);
+			fis = new FileInputStream (pepejava);
+			ois = new ObjectInputStream (fis);
 			
 			pAux = ois.readObject();
 			
 			if (pAux instanceof Persona)
 			{
-				for (int i = 0; i < pepejava.length() && pAux != null; i++)
+				while (true) //wtf
 				{
 					System.out.println(((Persona) pAux).toString ());
 					
@@ -79,17 +84,37 @@ public class GestoraHospital
 				}
 			}
 			
-			ois.close();
-		}
-		
-		catch (IOException e)
-		{
-			System.out.println("fail");
 		}
 		
 		catch (ClassNotFoundException e)
 		{
 			System.out.println("ClassNotFound");
+		}
+		
+		catch (EOFException e)
+		{
+			System.out.println("xd");
+		}
+		
+		catch (IOException e)
+		{
+			System.out.println("s");
+		}
+		
+		finally
+		{
+			try
+			{
+				if (ois != null)
+				{
+					ois.close ();
+				}
+			}
+			
+			catch (IOException e)
+			{
+				System.out.println("IOE");
+			}
 		}
 	}
 	//pruebas
